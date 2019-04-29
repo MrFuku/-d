e403e55bdf1a929c9ac8eda7833314ede4c98b8f
fuku <fuku@MacBook-Air.local>
2019-04-29 19:27:13 +0900

    apiレスポンス修正

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,19 @@
 class PageController < ApplicationController
 
   def index
+    # uri = URI.parse("http://18.182.76.60/api")
+    # response = Net::HTTP.start(uri.host, uri.port) do |http|
+    #   http.open_timeout = 5
+    #   http.read_timeout = 10
+    #   http.get(uri.request_uri)
+    # end
+    # @img
+    # case response
+    # when Net::HTTPSuccess
+    #   @img = JSON.parse(response.body)
+    #   # template = render "/api/ad_template", @img
+    #   # binding.pry
+    # end
   end
 
   def ads
