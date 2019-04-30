3dcd4592e52c712a6e5cec5aebba37616bf3aeed
fuku <fuku@MacBook-Air.local>
2019-04-30 17:10:30 +0900

    Hit Builderタグ実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'ad_new', to: 'page#ad_new'
   post 'ad_create', to: 'page#ad_create'
   get 'api', to: 'api#test'
+  get 'ad_request', to: 'page#ad_request'
   root to: 'page#index'
 end
